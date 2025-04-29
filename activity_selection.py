768df3a5b30b4aff708a927b9de7c694903a3dfc
PHILIP THOMAS <thomasphil6889@gmail.com>
2025-04-29 06:30:11 +0530

    Create activity_selection.py

---
+++ activity_selection.py
@@ -0,0 +1,18 @@
+def activity_selection(activities):
+    # Sort by end time
+    activities.sort(key=lambda x: x[1])
+    
+    selected = []
+    last_end_time = -1
+    
+    for start, end in activities:
+        if start > last_end_time:
+            selected.append((start, end))
+            last_end_time = end
+    
+    return selected
+
+# Example
+activities = [(1, 4), (3, 5), (0, 6), (5, 7), (8, 9), (5, 9)]
+selected = activity_selection(activities)
+print("Selected Activities:", selected)
