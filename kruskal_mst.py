5b55739168c17e10759ae52ad0c9c773f943a79f
PHILIP THOMAS <thomasphil6889@gmail.com>
2025-06-08 23:59:54 +0530

    Update kruskal_mst.py

--- kruskal_mst.py
+++ kruskal_mst.py
@@ -1,1 +1,20 @@
+# Minimum Spanning Tree using Kruskal's Algorithm
+def kruskal_mst(V, edges):
+    parent = list(range(V))
+
+    def find(u):
+        while parent[u] != u:
+            u = parent[u]
+        return u
+
+    def union(u, v):
+        parent[find(u)] = find(v)
+
+    mst_weight = 0
+    edges.sort(key=lambda x: x[2])
+    for u, v, w in edges:
+        if find(u) != find(v):
+            union(u, v)
+            mst_weight += w
+    return mst_weight
 
