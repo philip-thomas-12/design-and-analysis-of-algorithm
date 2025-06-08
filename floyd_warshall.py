b66f0c0987349851e5eb799bd353815a3d5ed337
PHILIP THOMAS <thomasphil6889@gmail.com>
2025-06-08 23:57:22 +0530

    Update floyd_warshall.py

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -1,1 +1,11 @@
+# All-pairs shortest path using Floyd-Warshall Algorithm
+def floyd_warshall(graph):
+    V = len(graph)
+    dist = [[graph[i][j] for j in range(V)] for i in range(V)]
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    return dist
 
